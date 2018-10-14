72e0feb1ef80d934afa4ef656e01593f3a80e92f
Tomas Vallejos <tomas.vallejos12@gmail.com>
2018-10-14 00:48:36 -0300

    reemplazo listo y limpio

--- t1.c
+++ t1.c
@@ -34,6 +34,45 @@ uint borrar_bits(uint x, uint pat, int len){
 	return x-suma;		   // se retorna el int sin los bits del patron
 }
 
+char *reemplazo(char *s, char c, char *pal){
+	int contador=0; // cuantas veces aparece c
+	int lens=0;		// largo de s
+	while(*s != '\0'){
+		if(*s==c){
+					contador++; // se suma 1 cada vez que se ve c
+		}
+		lens++;
+		s++;
+	}
+	s-=lens;					// se devuelve el puntero al principio del string
+	
+	int lenpal  = 0;			// contador del largo de la palabra
+	while(*pal != '\0'){
+		lenpal++;
+		pal++;
+	}
+	pal-=lenpal;				// se devuelve el puntero al principio del string
+
+	char * nueva = malloc(lens+contador*lenpal+1); // se pide memoria
+	char * pnueva = & *nueva;						// puntero por si se borran letras
+	while(*s != '\0'){
+		if (*s == c){
+			for (int i = 0; i < lenpal; ++i){		// se copia la palabra si se ve
+				* (pnueva+i)= * (pal+i);
+			}
+			pnueva+=lenpal;
+			
+		}else{
+			*pnueva=*s;								// se copia la misma letra si no 'c'
+			pnueva++;
+		}
+		s++;
+	}
+
+	*(++pnueva)='\0';								// se agrega la terminacion del string
+	
+	return nueva; 
+}
 
 /*int main(int argc, char const *argv[])
 

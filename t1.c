6635a5855b4a7618b179bbb15342a951e1f31622
Tomas Vallejos <tomas.vallejos12@gmail.com>
2018-10-13 22:36:30 -0300

    borrar bits aprobado y limpio

--- t1.c
+++ t1.c
@@ -18,38 +18,25 @@ void bin(int num)
 }
 
 uint borrar_bits(uint x, uint pat, int len){
-	uint mask=(1<<len)-1;
-	mask<<=(32-len);
-	pat<<=(32-len);
-	uint suma=0;
 	int num = 8*sizeof(int);
-	while(num>0+len-1){
-		//bin(mask);
-		//bin(pat);
+	uint mask=(1<<len)-1; // se crea una mascara para obtener solo los len bits de mas a la izq de x
+	mask<<=(num-len);
+	pat<<=(num-len);       // se empuja el patron hacia la izq
+	uint suma=0;		  
+	while(num>0+len-1){    // mientras el patron sigue "dentro" del entero lo empujo uno a la derecha
 		if((mask&x)==pat){
-			suma+=pat;
+			suma+=pat;	   // si el patron es encontrado se suma al acumulado
 		}
 		mask>>=1;
 		pat>>=1;
 		num--;
 	}
-	printf("resultado %u\n",x-suma );
-	return x-suma;
+	return x-suma;		   // se retorna el int sin los bits del patron
 }
 
 
 /*int main(int argc, char const *argv[])
-{
 
-	uint x = 137;
-	uint y = 473;
-	uint z = 2;
-	uint k = 5;
-	uint t = 1;
-	int prueba= 2 == z;
-	borrar_bits(x,t,1);
-	//borrar_bits(y,z,2);
-	//borrar_bitss(y,k,3);
-	
-	return 0;
+
+
 }*/
